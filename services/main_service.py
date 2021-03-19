32a5e6d97bb2c323e99124eb27aeb925ea7cf12e
kairick <kairickaav@gmail.com>
2021-03-19 17:01:09 +0300

    add charts

--- services/main_service.py
+++ services/main_service.py
@@ -1,7 +1,35 @@
+import datetime
+import json
 import os
+import pandas as pd
+import plotly
 from decouple import config
+import plotly.graph_objs as go
 
 
 def get_all_stocks():
     stocks = os.listdir(config('PATH_TO_STOCKS_DATA'))
     return [(stock, stock.split('.')[0]) for stock in stocks]
+
+
+def get_stock_chart(stock):
+    stocks_path = config('PATH_TO_STOCKS_DATA')
+    full_path = os.path.join(stocks_path, stock)
+    stock_file = pd.read_csv(full_path)
+    return get_chart(stock_file)
+
+
+def get_chart(csv_data):
+    json_datasets = []
+    csv_2019 = csv_data[(csv_data['DATE'] > 20190201) & (csv_data['DATE'] < 20200201)]
+    csv_2020 = csv_data[(csv_data['DATE'] > 20200201) & (csv_data['DATE'] < 20220201)]
+    for dataset in [csv_data, csv_2019, csv_2020]:
+        trace = go.Scatter(x=dataset["Unnamed: 0"], y=dataset["PRICE"])
+        layout = go.Layout(xaxis=dict(title=""),
+                           yaxis=dict(title="Цена"), )
+        data = [trace]
+        fig = go.Figure(data=data, layout=layout)
+        fig_json = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+        json_datasets.append(fig_json)
+    return json_datasets
+
--- webservice/routes.py
+++ webservice/routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, request
 
+from services.main_service import get_stock_chart
 from webservice.forms import ChooseStockForm
 
 main_blueprint = Blueprint('main_blueprint', __name__, static_folder='webservice/static',
@@ -9,12 +10,13 @@ main_blueprint = Blueprint('main_blueprint', __name__, static_folder='webservice
 @main_blueprint.route('/')
 def main():
     form = ChooseStockForm()
-    return render_template('index.html', form=form, chart=None)
+    return render_template('index.html', form=form, chart=None, stock=None)
 
 
-@main_blueprint.route('/get_stock_chart', methods=['POST'])
-def get_stock_chart():
+@main_blueprint.route('/stock_chart', methods=['POST'])
+def stock_chart():
     stock = request.form.get('stock')
-
+    chart = get_stock_chart(stock)
     form = ChooseStockForm()
-    return render_template('index.html', form=form, chart=None)
+    return render_template('index.html', form=form, chart=chart, stock=stock.split('.')[0])
+
